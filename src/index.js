d63385c7a6528f74f77ca1b2b893928b3c0e2289
Anadroid <anajelicic89@gmail.com>
2024-03-04 09:21:20 +0100

    Some logic

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import "./index.css";
 import {MaptilerLayer} from "@maptiler/leaflet-maptilersdk";
 
 
-
-
 const defaultBackgroundMapStyle = 3;
 
 const circleMarkerOptions = {
@@ -34,7 +32,6 @@ const featureMarker = (feature, latlng) => {
   });
   return marker;
 };
-
 
 var map = L.map("map").setView([52.5105, 13.4061], 12);
 map.setMinZoom(10);
@@ -67,7 +64,59 @@ styles.forEach((s) => {
 
 baseLayers[defaultBackgroundMapStyle].addTo(map);
 
+document.getElementById("wheelchair_button").addEventListener("click", wheelchairSelected);
+
+function wheelchairSelected() {
+    fetch(new Request("/map_data.geojson")).then((response) => response.json()).then((data) => {
+
+  data.forEach((o) => {
+    if (o.properties['wheelchair']=== 'yes') {
+    overlay.addData(o)
+
+    //creating the bookstores result list
+    const details = document.createElement('details');
+    details.className = "result_item";
+    const summary = document.createElement('summary');
+    summary.textContent = o.properties['name'];
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+
+    p1.textContent = o.properties['addr:street'] + ' ' + o.properties['addr:housenumber'] + 
+                  ', ' + o.properties['addr:postcode'] + ' '  + o.properties['addr:city'];
+    //needs to be differently formated
+    p2.textContent = o.properties['opening_hours'];
+
+    details.appendChild(summary);
+    details.appendChild(p1);
+    details.appendChild(p2);
+    document.getElementById('results').appendChild(details);
+  }
+  });
+});
+}
+
 // https://leafletjs.com/examples/geojson/
 fetch(new Request("/map_data.geojson")).then((response) => response.json()).then((data) => {
-  data.forEach((o) => overlay.addData(o));
+
+  data.forEach((o) => {
+    overlay.addData(o)
+
+    //creating the bookstores result list
+    const details = document.createElement('details');
+    details.className = "result_item";
+    const summary = document.createElement('summary');
+    summary.textContent = o.properties['name'];
+    const p1 = document.createElement('p');
+    const p2 = document.createElement('p');
+
+    p1.textContent = o.properties['addr:street'] + ' ' + o.properties['addr:housenumber'] + 
+                  ', ' + o.properties['addr:postcode'] + ' '  + o.properties['addr:city'];
+    //needs to be differently formated
+    p2.textContent = o.properties['opening_hours'];
+
+    details.appendChild(summary);
+    details.appendChild(p1);
+    details.appendChild(p2);
+    document.getElementById('results').appendChild(details);
+  });
 });
